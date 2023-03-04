528f4fbff97488726cb530c76ab8f7aec3903387
uvcourses <uvcourses@gmail.com>
2023-03-04 12:03:16 -0600

    first commit

---
+++ src/main/java/com/learn/dependencykeeper/DependencyKeeperApplication.java
@@ -0,0 +1,13 @@
+	package com.learn.dependencykeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/dependencykeeper/DependencyKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.dependencykeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
